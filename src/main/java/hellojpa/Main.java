d94c032108684ddb21f2c4d1d1bb31987bce473a
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2022-12-13 10:27:34 +0900

    엔티티 매핑 실습

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -15,7 +15,23 @@ public class Main {
     tx.begin();
 
     try {
+      Member member1 = new Member();
+      member1.setUsername("kim");
 
+      Member member2 = new Member();
+      member2.setUsername("lee");
+
+      Member member3 = new Member();
+      member3.setUsername("park");
+
+      System.out.println("=========================");
+      em.persist(member1); // db seq -> 1, 51
+      em.persist(member2); // 메모리에서 호출
+      em.persist(member3); // 메모리에서 호출
+      System.out.println("member1의 id 값: " + member1.getId());
+      System.out.println("member2의 id 값: " + member2.getId());
+      System.out.println("member3의 id 값: " + member3.getId());
+      System.out.println("=========================");
       tx.commit();
 
     } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -4,23 +4,33 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 50)
 public class Member {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
   private Long id;
   @Column(name = "name")
   private String username;
 
-  private Integer age;
-  @Enumerated(EnumType.STRING)
-  private RoleType roleType;
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date createdDate;
-  @Temporal(TemporalType.TIMESTAMP)
-  private Date lastModifiedDate;
-  @Lob
-  private String description;
-
   protected Member() {}
 
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
 }
