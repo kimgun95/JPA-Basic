06ecd02518d1a5ef2e9a1f99261aa3103986b31e
kimgun95 <harrison1995@naver.com>
2023-05-02 20:57:01 +0900

    엔티티 설계 및 매핑

---
+++ src/main/java/hellojpa/domain/Order.java
@@ -0,0 +1,55 @@
+package hellojpa.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "ORDERS")
+public class Order {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "ORDER_ID")
+  private Long id;
+  @ManyToOne
+  @JoinColumn(name = "MEMBER_ID")
+  private Member member;
+  private LocalDateTime orderDate;
+  @Enumerated(EnumType.STRING)
+  private OrderStatus orderStatus;
+  @OneToMany(mappedBy = "order")
+  private List<OrderItem> orderItems = new ArrayList<>();
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Member getMember() {
+    return member;
+  }
+
+  public void setMember(Member member) {
+    this.member = member;
+  }
+
+  public LocalDateTime getOrderDate() {
+    return orderDate;
+  }
+
+  public void setOrderDate(LocalDateTime orderDate) {
+    this.orderDate = orderDate;
+  }
+
+  public OrderStatus getOrderStatus() {
+    return orderStatus;
+  }
+
+  public void setOrderStatus(OrderStatus orderStatus) {
+    this.orderStatus = orderStatus;
+  }
+}
